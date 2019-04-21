1abed4428ad121178a0b89b114db711a57d27db7
Michael Symmes <msymmes@gmail.com>
2019-04-21 14:51:40 -0400

    Highlight when changing source/destination.

--- src/Location.jsx
+++ src/Location.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { colors } from "./config";
 
-const Location = ({ coords, type, settings, dispatch }) => {
+const Location = ({ coords, type, settings, highlight, dispatch }) => {
   return (
     <rect
       x={coords.x * settings.tileSize}
       y={coords.y * settings.tileSize}
       width={settings.tileSize}
       height={settings.tileSize}
-      fill={colors[type]}
+      fill={highlight ? "lightblue" : colors[type]}
       stroke="grey"
       onClick={() => {
         dispatch({
