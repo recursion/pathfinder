01d0846dba0881c8f4c1b8aab0a1f487b0b22d01
Michael Symmes <msymmes@gmail.com>
2019-04-21 16:13:32 -0400

    Improve continual painting method.

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -42,78 +42,91 @@ const init = () => {
   };
 };
 
+const movePiece = (state, action) => {
+  const coords = JSON.stringify(action.coords);
+  // dont allow replacing source with destination or vice-versa
+  if (
+    (state.action.locationType === DESTINATION &&
+      state.grid[coords] === SOURCE) ||
+    (state.action.locationType === SOURCE && state.grid[coords] === DESTINATION)
+  ) {
+    return state;
+  }
+  const nextGrid = {
+    ...state.grid,
+    [state.action.position]: EMPTY,
+    [coords]: state.action.locationType
+  };
+  return { ...state, grid: nextGrid, action: initialState.action };
+};
+
+const paint = (state, action) => {
+  const coords = JSON.stringify(action.coords);
+  const paintType = action.locationType === WALL ? EMPTY : WALL;
+  return {
+    ...state,
+    grid: {
+      ...state.grid,
+      [coords]: paintType
+    },
+    action: {
+      type:
+        action.locationType === SOURCE || action.locationType === DESTINATION
+          ? "MOVE"
+          : `PAINT_${paintType}`,
+      locationType: action.locationType,
+      position: coords
+    }
+  };
+};
+
 const reducer = (state, action) => {
   const coords = JSON.stringify(action.coords);
   switch (action.type) {
     case "START_PAINT":
       switch (state.action.type) {
         case "MOVE":
-          // dont allow replacing source with destination or vice-versa
-          if (
-            state.action.locationType === DESTINATION &&
-            state.grid[coords] === SOURCE
-          ) {
-            return state;
-          }
-          if (
-            state.action.locationType === SOURCE &&
-            state.grid[coords] === DESTINATION
-          ) {
-            return state;
-          }
-          const nextGrid = {
-            ...state.grid,
-            [state.action.position]: EMPTY,
-            [coords]: state.action.locationType
-          };
-          return { ...state, grid: nextGrid, action: initialState.action };
+          return movePiece(state, action);
         case "NONE":
-          return {
-            ...state,
-            grid: {
-              ...state.grid,
-              [coords]: action.locationType === WALL ? EMPTY : WALL
-            },
-            action: {
-              type:
-                action.locationType === SOURCE ||
-                action.locationType === DESTINATION
-                  ? "MOVE"
-                  : "PAINT",
-              locationType: action.locationType,
-              position: coords
-            }
-          };
+          return paint(state, action);
         default:
+          console.error(`Unhandled action ${action.type} ${state.action.type}`);
           return state;
       }
 
     case "PAINT":
-      if (state.action.type !== "PAINT") return state;
+      if (!state.action.type.includes("PAINT")) return state;
+      const paintType = state.action.type.includes("WALL") ? "WALL" : "EMPTY";
       switch (action.locationType) {
         case DESTINATION:
         case SOURCE:
           return state;
         case WALL:
-          return { ...state, grid: { ...state.grid, [coords]: EMPTY } };
         case EMPTY:
-          return { ...state, grid: { ...state.grid, [coords]: WALL } };
+          return { ...state, grid: { ...state.grid, [coords]: paintType } };
         default:
+          console.error(
+            `Unhandled action ${action.type} ${state.action.locationType}`
+          );
           return state;
       }
 
     case "STOP_PAINT":
       switch (state.action.type) {
         case "MOVE":
+        case "NONE":
           return state;
-        case "PAINT":
+        case "PAINT_WALL":
+        case "PAINT_EMPTY":
           // stop painting
           return { ...state, action: initialState.action };
         default:
+          console.error(`Unhandled action ${action.type} ${state.action.type}`);
           return state;
       }
 
     default:
+      console.error(`Unhandled action ${action.type}`);
       return state;
   }
 };
